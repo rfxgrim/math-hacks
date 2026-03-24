19492b54f74de044fbdecb6402bd3294aa994ac9
jake <rexlex32@outlook.com>
2026-03-24 17:18:04 -0400

    iosehfihjoiwe

--- sw.js
+++ sw.js
@@ -4,13 +4,10 @@ const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.startsWith(self.location.origin + "/scramjet/")) {
+  if (scramjet.route(event)) {
     event.respondWith((async () => {
       await scramjet.loadConfig();
-      if (scramjet.route(event)) {
-        return scramjet.fetch(event);
-      }
-      return fetch(event.request);
+      return scramjet.fetch(event);
     })());
   }
 });
