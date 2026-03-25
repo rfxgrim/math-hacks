f129c08fbfed893b1da9600fd3edd514e4c4801e
jake <rexlex32@outlook.com>
2026-03-25 19:28:49 -0400

    wpjfpjwpej4fp

--- sw.js
+++ sw.js
@@ -3,24 +3,17 @@ importScripts("/assets/scramjet/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
-scramjet.config = {
-  prefix: "/scramjet/",
-};
-
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
   if (url.pathname.startsWith("/assets/")) return;
   if (url.pathname.startsWith("/css/")) return;
   if (url.pathname.startsWith("/js/")) return;
   if (url.pathname === "/sw.js") return;
-  if (url.pathname === "/" || url.pathname === "/index.html") return;
   if (url.pathname === "/go.html") return;
+  if (url.pathname === "/" || url.pathname === "/index.html") return;
   if (!url.pathname.startsWith("/scramjet/")) return;
 
   if (scramjet.route(event)) {
-    event.respondWith((async () => {
-      await scramjet.loadConfig();
-      return scramjet.fetch(event);
-    })());
+    event.respondWith(scramjet.fetch(event));
   }
 });
