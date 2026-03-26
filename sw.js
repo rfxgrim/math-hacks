261877e8f12cb63c8eb654d7ff3a2dd298a29f3e
jake <rexlex32@outlook.com>
2026-03-26 17:50:17 -0400

    siuehfiwmneofjsef

--- sw.js
+++ sw.js
@@ -3,8 +3,34 @@ importScripts("/assets/scramjet/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
+scramjet.config = {
+  prefix: "/scramjet/",
+  files: {
+    wasm: "/assets/scramjet/scramjet.wasm.wasm",
+    all: "/assets/scramjet/scramjet.all.js",
+    sync: "/assets/scramjet/scramjet.sync.js",
+  },
+  flags: {
+    serviceworkers: false,
+    syncxhr: false,
+    strictRewrites: true,
+    rewriterLogs: false,
+    captureErrors: true,
+    cleanErrors: false,
+    scramitize: false,
+    sourcemaps: true,
+    destructureRewrites: false,
+    interceptDownloads: false,
+    allowInvalidJs: true,
+    allowFailedIntercepts: true,
+  },
+  codec: {
+    encode: "e=>e?encodeURIComponent(e):e",
+    decode: "e=>e?decodeURIComponent(e):e",
+  },
+};
+
 async function handleRequest(event) {
-  await scramjet.loadConfig();
   if (scramjet.route(event)) {
     return scramjet.fetch(event);
   }
