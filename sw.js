801ea0b8ac21ee8c4d21975bda2f25830f0273bd
jake <rexlex32@outlook.com>
2026-03-28 16:11:54 -0400

    8jsijfiowjeormosjef

--- sw.js
+++ sw.js
@@ -3,24 +3,12 @@ importScripts("/assets/scramjet/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
-async function waitForConfig() {
-  for (let i = 0; i < 20; i++) {
-    await scramjet.loadConfig();
-    if (scramjet.config) return;
-    await new Promise(r => setTimeout(r, 300));
-  }
-}
-
-async function handleRequest(event) {
-  if (!scramjet.config) {
-    await waitForConfig();
-  }
-  if (scramjet.route(event)) {
-    return scramjet.fetch(event);
-  }
-  return fetch(event.request);
-}
-
 self.addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event));
+  event.respondWith(async () => {
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) {
+      return scramjet.fetch(event);
+    }
+    return fetch(event.request);
+  });
 });
